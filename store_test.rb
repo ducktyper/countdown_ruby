94b7e5befe16570af678cd0ee83cbd5f0a02018a
ducksan cho <ducktyper@gmail.com>
2015-08-13 11:54:26 +1200

    TODO: rename item to product (more natural)

--- store_test.rb
+++ store_test.rb
@@ -3,20 +3,20 @@ require './store'
 
 describe "store" do
 
-  it "starts with 0 item" do
+  it "starts with 0 product" do
     store = Store.new
-    assert_equal(0, store.item_count())
+    assert_equal(0, store.product_count())
   end
 
-  it "can register item" do
+  it "can register product" do
     store = Store.new
 
     barcode = "001"
     name    = "apple"
     price   = 5
 
-    store.register_item(barcode, name, price)
-    assert_equal(1, store.item_count())
+    store.register_product(barcode, name, price)
+    assert_equal(1, store.product_count())
   end
 
 end
