6f376c9a4cd0b927cffd96b3ee56f4b2239627c8
ducksan cho <ducktyper@gmail.com>
2015-08-13 11:52:31 +1200

    TODO: store can register item

--- store_test.rb
+++ store_test.rb
@@ -8,4 +8,15 @@ describe "store" do
     assert_equal(0, store.item_count())
   end
 
+  it "can register item" do
+    store = Store.new
+
+    barcode = "001"
+    name    = "apple"
+    price   = 5
+
+    store.register_item(barcode, name, price)
+    assert_equal(1, store.item_count())
+  end
+
 end
