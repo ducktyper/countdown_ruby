11dab1f8364bd12e98dc4fd4fcceda0a3974113a
ducksan cho <ducktyper@gmail.com>
2015-08-13 15:32:11 +1200

    __rename variables with type (each to read)

--- store.rb
+++ store.rb
@@ -1,41 +1,41 @@
 class Store
   def initialize
-    @products = []
-    @purchases = []
+    @product_array = []
+    @purchase_array = []
   end
 
   def register_product(barcode, name, price)
     new_product = [barcode, name, price]
-    @products << new_product
+    @product_array << new_product
   end
 
   def product_count
-    @products.size
+    @product_array.size
   end
 
-  def calculate_cost(barcodes)
+  def calculate_cost(barcode_array)
     cost = 0
-    barcodes.each do |barcode|
-      product = @products.find {|product| product[0] == barcode}
+    barcode_array.each do |barcode|
+      product = @product_array.find {|product| product[0] == barcode}
       cost = cost + product[2]
     end
     cost
   end
 
-  def receipt(barcodes)
+  def receipt(barcode_array)
     result = ""
-    barcodes.each do |barcode|
-      product = @products.find {|product| product[0] == barcode}
+    barcode_array.each do |barcode|
+      product = @product_array.find {|product| product[0] == barcode}
       name = product[1]
       cost = product[2]
       result = result + "#{name} $#{cost}\n"
     end
-    result = result + "total $#{calculate_cost(barcodes)}"
+    result = result + "total $#{calculate_cost(barcode_array)}"
     result
   end
 
   def purchase(barcodes_array)
-    @purchases << barcodes_array
+    @purchase_array << barcodes_array
     receipt(barcodes_array)
   end
 
