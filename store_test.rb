2b93179d845242829d6a8c58da95da6a31aae09b
ducksan cho <ducktyper@gmail.com>
2015-08-13 12:01:01 +1200

    TODO: store can calculate total cost of given barcodes

--- store_test.rb
+++ store_test.rb
@@ -19,4 +19,11 @@ describe "store" do
     assert_equal(1, store.product_count())
   end
 
+  it "can caculate total cost of given barcodes" do
+    store = Store.new
+    store.register_product("001", "apple", 10)
+    store.register_product("002", "orange", 20)
+    assert_equal(40, store.calculate_cost(["001", "001", "002"]))
+  end
+
 end
